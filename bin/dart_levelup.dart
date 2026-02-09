ecab4228239f36d2282e2fb6764f81f32fe276fb
levyrodrigues23 <joselevyrodrigues23@gmail.com>
2026-02-09 18:28:45 -0300

    feat: arquivos principais adicionados

---
+++ bin/dart_levelup.dart
@@ -0,0 +1,5 @@
+import 'package:dart_levelup/dart_levelup.dart' as dart_levelup;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_levelup.calculate()}!');
+}
---
+++ test/dart_levelup_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_levelup/dart_levelup.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
